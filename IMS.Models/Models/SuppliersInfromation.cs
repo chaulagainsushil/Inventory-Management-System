f3f209b469351a7c9f9e61f99741df60cad46445
chaulagainsushil <122629534+chaulagainsushil@users.noreply.github.com>
2025-10-08 18:11:19 +0545

    This is first Commit .

---
+++ IMS.Models/Models/SuppliersInfromation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Models.Models
+{
+    public class SuppliersInfromation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
